75ed9b8710c0b8684ffaf01e1faad9dad0debf69
Golovin Pavel <gopavel0@gmail.com>
2018-04-12 23:57:21 +0300

    add check for axioms and hypothesis

--- hw1/src/Main.hs
+++ hw1/src/Main.hs
@@ -7,9 +7,10 @@ import Data.List.Split
 import Grammar
 import Lexer (alexScanTokens)
 import Parser (parseExpr)
+import Data.Map.Strict (Map, insert, insertWith, empty, fromList, lookup)
 
 inputFile :: String
-inputFile = "axioms.txt"
+inputFile = "test1.txt"
 
 outputFile :: String
 outputFile = "output.txt"
@@ -30,8 +31,9 @@ data Prop = Prop {
     strExpr :: String,
     expr :: Expr,
     indexOfAxiom :: Maybe Int,
-    indexOfHypo :: Maybe Bool,
+    indexOfHypo :: Maybe Int,
     mp :: Maybe (Int, Int) }
+    deriving (Eq, Ord)
 
 getAnnotate :: Prop -> String
 getAnnotate prop = case indexOfAxiom prop of
@@ -69,12 +71,29 @@ strBefore sep str = let (Just index) = findIndex (== '-') str
 -- annotate :: [String] -> -> [String]
 -- annotate hypos conclusion prots
 
-isAxiom :: Expr -> Bool
-isAxiom (Binary Impl a b) = True
-isAxiom _ = False
+isAxiom :: Expr -> Maybe Int
+isAxiom (Binary Impl (Binary Impl a1 b1) (Binary Impl (Binary Impl a2 (Binary Impl b2 c1)) (Binary Impl a3 c2))) = if a1 == a2 && a2 == a3 && b1 == b2 && c1 == c2 then (Just 2) else Nothing
+isAxiom (Binary Impl a1 (Binary Impl b1 (Binary And a2 b2))) = if a1 == a2 && b1 == b2 then (Just 3) else Nothing
+isAxiom (Binary Impl (Binary And a b) c) =
+    if a == c then (Just 4)
+    else if b == c then (Just 5)
+    else Nothing
+isAxiom (Binary Impl c (Binary Or a b)) =
+    if c == a then (Just 6)
+    else if c == b then (Just 7)
+    else Nothing
+isAxiom (Binary Impl (Binary Impl a1 c1) (Binary Impl (Binary Impl b1 c2) (Binary Impl (Binary Or a2 b2) c3))) = if a1 == a2 && c1 == c2 && c2 == c3 && b1 == b2 then (Just 8) else Nothing
+isAxiom (Binary Impl (Binary Impl a1 b1) (Binary Impl (Binary Impl a2 (Not b2)) (Not a3))) = if a1 == a2 && a2 == a3 && b1 == b2 then (Just 9) else Nothing
+isAxiom (Binary Impl (Not (Not a1)) a2) = if a1 == a2 then (Just 10) else Nothing
+isAxiom (Binary Impl a1 (Binary Impl b1 a2)) = if a1 == a2 then (Just 1) else Nothing
+isAxiom _  = Nothing
 
--- annotateAxiom :: Prop -> Prop
--- annotateAxiom prop
+
+annotateAxiom :: Prop -> Prop
+annotateAxiom prop@(Prop i s e ia ih ismp) = (Prop i s e (isAxiom $ expr prop) ih ismp)
+
+annotateHypos :: Map Expr Int -> Prop -> Prop
+annotateHypos mapOfHypos prop@(Prop i s e ia ih ismp) = (Prop i s e ia (Data.Map.Strict.lookup e mapOfHypos) ismp)
 
 main :: IO ()
 main = do
@@ -84,13 +103,18 @@ main = do
     headFile <- return $ splitOn "|-" (head lineOfFiles)
     lineOfFiles <- return $ drop 1 lineOfFiles
     hypos <- return $ splitOn "," (head headFile)
+    hypos <- return $ map getExpr hypos
+    hypos <- return $ fromList (zip hypos ([1..]::[Int]))
     conclusion <- return $ last headFile
     props <- return $ toProps lineOfFiles
-    writeFile outputFile $ "\nHypos:\n" ++ show hypos
-    appendFile outputFile $ "\nconclusion:\n" ++ conclusion
-    appendFile outputFile $ "\nprops:\n" ++ intercalate "\n" (map show props)
-    appendFile outputFile $ intercalate "\n" (map (show . getExpr) lineOfFiles)
-    -- appendFile outputFile (intercalate "\n" annotate hypos conclusion prots)
+    props <- return $ map annotateAxiom props
+    props <- return $ map (annotateHypos hypos) props
+    writeFile outputFile $ ""
+    -- writeFile outputFile $ "\nHypos:\n" ++ show hypos
+    -- appendFile outputFile $ "\nconclusion:\n" ++ conclusion
+    -- appendFile outputFile $ "\nprops:\n" ++ intercalate "\n" (map show props)
+    -- appendFile outputFile $ intercalate "\n" (map (show . getExpr) lineOfFiles)
+    appendFile outputFile (intercalate "\n" (map show props))
 --1:[2] 2:[] []
 -- [1..60000] <=> init: 1, inc : (+ 1)
 -- summ acc [] = acc
